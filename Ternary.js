933ec78e358ff2d7fdd689104c533f05db804253
Ingomoboere Ekeuwei <Ingomoboere@gmail.com>
2025-09-28 18:51:53 +0100

    Ternary.js

--- Ternary.js
+++ Ternary.js
@@ -1,5 +1,6 @@
 //TERNARY OPERATOR
-// The ternary operator is a shorter way of writing an if...else statement I javascript. It is called ternary because it is divided into three parts: a condition, a value if true, and a value if false.
+// The ternary operator is a shorter way of writing an if...else statement I javascript. 
+//It is called ternary because it is divided into three parts: a condition, a value if true, and a value if false.
 
 let age = 18;
 let status = age >= 18 ? "Adult" : "Minor";
