485d4d4b91ef4082b10ec8e0e47ab82bf5f9b03c
Ingomoboere Ekeuwei <Ingomoboere@gmail.com>
2025-09-28 18:41:52 +0100

    Array module.js

--- Array module.js
+++ Array module.js
@@ -1,5 +1,8 @@
 //ARRAY MODULE
-// Arrays are a collection of data of similar or same data types. These can be accessed and modified and can come in different forms. Array functions are built-in functions in javascript that help you manipulate arrays. Examples: push(), unshift(), pop(), shift(), indexOf(), includes(), reverse(), sort().
+// Arrays are a collection of data of similar or same data types. 
+//These can be accessed and modified and can come in different forms.
+//Array functions are built-in functions in javascript that help you manipulate arrays. 
+//Examples: push(), unshift(), pop(), shift(), indexOf(), includes(), reverse(), sort().
 
 // Create an Array of student
 let students = ["Alice", "Bob", "Charlie"];
