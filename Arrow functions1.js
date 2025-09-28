b271bfb07da35ff635a13feb229fd7018cd91b45
Ingomoboere Ekeuwei <Ingomoboere@gmail.com>
2025-09-28 20:15:50 +0100

    Create Arrow functions1.js

---
+++ Arrow functions1.js
@@ -0,0 +1,8 @@
+//ARROW FUNCTION
+// An arrow function is a type of function in JavaScript that is defined using the => symbol.
+//It provides a shorter and cleaner way to write functions. It does not require the function keyword. 
+//If the function has a single expression, it can return the value implicitly without return. 
+
+const add = (a, b) =>  a + b; 
+
+console.log(add(5, 3));
