2e04682eccf971b8738758ba4e4092249b5aa8e7
Ingomoboere Ekeuwei <Ingomoboere@gmail.com>
2025-09-28 18:50:15 +0100

    Modules.js

--- Modules.js
+++ Modules.js
@@ -1,5 +1,6 @@
 //MODULES
-// A module in JavaScript is a file that contains code (like functions, objects, or variables) which can be imported and used in other files.It helps organize code into smaller, reusable pieces.Instead of putting all code in one file, you can split functionality across multiple modules.
+// A module in JavaScript is a file that contains code (like functions, objects, or variables) which can be imported and used in other files.
+//It helps organize code into smaller, reusable pieces.Instead of putting all code in one file, you can split functionality across multiple modules.
 
 // My module
 const mathModule = {
