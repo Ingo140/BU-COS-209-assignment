8ad7a183351c7cdfcb37e21af1a50c75a462a164
Ingomoboere Ekeuwei <Ingomoboere@gmail.com>
2025-09-28 18:48:04 +0100

    Class.js

--- Class.js
+++ Class.js
@@ -1,5 +1,8 @@
 //CLASS
-// A class defines the information and actions that objects can use to perform tasks as required by the user. Classes help organize code and make it reusable. Use the constructor method to initialize properties when creating an object. Methods inside a class do not need the function keyword. Each object created from a class is called an instance and can have its own data. Classes support inheritance, allowing one class to extend another.
+// A class defines the information and actions that objects can use to perform tasks as required by the user. 
+//Classes help organize code and make it reusable. Use the constructor method to initialize properties when creating an object.
+//Methods inside a class do not need the function keyword. Each object created from a class is called an instance and can have its own data.
+//Classes support inheritance, allowing one class to extend another.
 
  class Add { sum(x, y) { return x + y; } } 
 
